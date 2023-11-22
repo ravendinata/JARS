b34c06623b77f00e0cc739c9fd3c5ed2e8ee7ead
Raven Limadinata <raven.limadinata@outlook.com>
2023-11-22 08:15:47 +0700

    Migrated to CustomTKinter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import openpyxl
 
 import pandas as pd
+import customtkinter as ctk
 import tkinter as tk
 
 from tkinter import filedialog, messagebox
@@ -47,12 +48,12 @@ def process_file(file_path, output_file_path, mode):
 # UI functions
 
 def browse_file():
-    file_path = filedialog.askopenfilename(defaultextension = ".csv", filetypes = [("Comma-Separated Value", "*.csv")])
+    file_path = tk.filedialog.askopenfilename(defaultextension = ".csv", filetypes = [("Comma-Separated Value", "*.csv")])
     txt_source_path.delete(0, tk.END)  # Remove current file path in entry
     txt_source_path.insert(0, file_path)  # Insert the file path into entry
 
 def save_file():
-    file_path = filedialog.asksaveasfilename(defaultextension = ".xlsx", filetypes = [("Microsoft Excel Document", "*.xlsx")])
+    file_path = tk.filedialog.asksaveasfilename(defaultextension = ".xlsx", filetypes = [("Microsoft Excel Document", "*.xlsx")])
     txt_output_path.delete(0, tk.END)  # Remove current file path in entry
     txt_output_path.insert(0, file_path)  # Insert the file path into entry
 
@@ -63,45 +64,50 @@ def process():
     try:
         # check_overwrite(output_file_path)
         process_file(source_file, output_file_path, mode)
-        messagebox.showinfo("Success", f"Done. Output file saved at {output_file_path}")
+        tk.messagebox.showinfo("Success", f"Done. Output file saved at {output_file_path}")
     except Exception as e:
-        messagebox.showerror("Error", str(e))
+        tk.messagebox.showerror("Error", str(e))
 
-root = tk.Tk()
+# Root window setup
+root = ctk.CTk()
 root.title("JARS Report Processor")
-root['padx'] = 10
-root['pady'] = 10
+
+root.columnconfigure(0, weight = 1)
+root.columnconfigure(1, weight = 2)
+root.columnconfigure(2, weight = 1)
 
 # Sourcefile path
-lbl_source = tk.Label(root, text = "Source file path:")
-txt_source_path = tk.Entry(root, width = 50)
-btn_browse_source = tk.Button(root, text = "Browse...", command = browse_file)
-
-lbl_source.grid(row = 0, column = 0, sticky = tk.W, pady = 2)
-txt_source_path.grid(row = 0, column = 1, sticky = tk.W, pady = 2)
-btn_browse_source.grid(row = 0, column = 2, sticky = tk.W, pady = 2, padx = 2)
+lbl_source = ctk.CTkLabel(root, text = "Source file path:")
+txt_source_path = ctk.CTkEntry(root, width = 250)
+btn_browse_source = ctk.CTkButton(root, text = "Browse...", width = 100, command = browse_file)
 
 # Mode
-lbl_mode = tk.Label(root, text="Report file type:")
+lbl_mode = ctk.CTkLabel(root, text="Report file type:")
 mode_var = tk.IntVar()
-rdo_mode_1 = tk.Radiobutton(root, text = "Cohort All Assignment Grades", variable = mode_var, value = 1)
-rdo_mode_2 = tk.Radiobutton(root, text = "Cohort All Final Grades", variable = mode_var, value = 2)
-
-lbl_mode.grid(row = 1, column = 0, sticky = tk.W, pady = 2)
-rdo_mode_1.grid(row = 2, column = 0, sticky = tk.W, pady = 2)
-rdo_mode_2.grid(row = 3, column = 0, sticky = tk.W, pady = 2)
+rdo_mode_1 = ctk.CTkRadioButton(root, text = "Cohort All Assignment Grades", variable = mode_var, value = 1)
+rdo_mode_2 = ctk.CTkRadioButton(root, text = "Cohort All Final Grades", variable = mode_var, value = 2)
 
 # Output file path
-lbl_output = tk.Label(root, text = "Output file path:")
-txt_output_path = tk.Entry(root, width = 50)
-btn_browse_output = tk.Button(root, text = "Browse...", command = save_file)
-
-lbl_output.grid(row = 4, column = 0, sticky = tk.W, pady = 2)
-txt_output_path.grid(row = 4, column = 1, sticky = tk.W, pady = 2)
-btn_browse_output.grid(row = 4, column = 2, sticky = tk.W, pady = 2, padx = 2)
+lbl_output = ctk.CTkLabel(root, text = "Output file path:")
+txt_output_path = ctk.CTkEntry(root, width = 250)
+btn_browse_output = ctk.CTkButton(root, text = "Browse...", width = 100, command = save_file)
 
 # Process button
-btn_process = tk.Button(root, text = "Process", command = process)
-btn_process.grid(row = 5, column = 2, sticky = tk.W, pady = 2)
+btn_process = ctk.CTkButton(root, text = "Process", width = 100, command = process)
+
+# Layout
+lbl_source.grid(row = 0, column = 0, sticky = tk.W, padx = 5, pady = 2)
+txt_source_path.grid(row = 0, column = 1, sticky = tk.W, pady = 2)
+btn_browse_source.grid(row = 0, column = 2, sticky = tk.EW, padx = 5, pady = 2)
+
+lbl_mode.grid(row = 1, column = 0, sticky = tk.W, padx = 5, pady = 2)
+rdo_mode_1.grid(row = 2, column = 0, columnspan = 2, sticky = tk.W, padx = 5, pady = 2)
+rdo_mode_2.grid(row = 3, column = 0, columnspan = 2, sticky = tk.W, padx = 5, pady = 2)
+
+lbl_output.grid(row = 4, column = 0, sticky = tk.W, padx = 5, pady = 2)
+txt_output_path.grid(row = 4, column = 1, sticky = tk.W, pady = 2)
+btn_browse_output.grid(row = 4, column = 2, sticky = tk.EW, padx = 5, pady = 2)
+
+btn_process.grid(row = 5, column = 2, sticky = tk.EW, padx = 5, pady = 10)
 
 root.mainloop()
