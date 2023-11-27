342db75683b579935151c408829152dee89c89ef
Raven Limadinata <raven.limadinata@outlook.com>
2023-11-27 14:23:04 +0700

    Formatting improvement
    - Fixed issue with conjunctive adverb causing spacing issue
    - Used f-string to concatenate 2 sentences instead of using join

--- processor/helper/comment_generator.py
+++ processor/helper/comment_generator.py
@@ -11,7 +11,7 @@ class CommentGenerator:
 
         self._comment_mapping = self._comment_mapping.fillna("")
 
-    def generate_comment(self):
+    def generate_comment(self, probe = False):
         pronoun = "he" if self._gender == "M" else "she"
         adjective = "his" if self._gender == "M" else "her"
 
@@ -29,11 +29,11 @@ class CommentGenerator:
             positive_comments = positive_comments.replace("also", "")
             positive_comments += f". {positive_sentences[-1]}"
         elif (len(positive_sentences) == 3):
-            positive_comments = " ".join([positive_sentences[0], ", and ", positive_sentences[1]])
+            positive_comments = f"{positive_sentences[0]}, and {positive_sentences[1]}"
             positive_comments = positive_comments.replace("also", "")
             positive_comments = ". ".join([positive_comments, positive_sentences[2]])
         elif (len(positive_sentences) == 2):
-            positive_comments = " ".join([positive_sentences[0], ", and ", positive_sentences[1]])
+            positive_comments = f"{positive_sentences[0]}, and {positive_sentences[1]}"
             positive_comments = positive_comments.replace("also", "", 1)
         elif (len(positive_sentences) == 1):
             positive_comments = positive_sentences[0]
@@ -46,11 +46,11 @@ class CommentGenerator:
             negative_comments = negative_comments.replace("also", "")
             negative_comments += f". Further, {negative_sentences[-1]}"
         elif (len(negative_sentences) == 3):
-            negative_comments = " ".join([negative_sentences[0], ", and ", negative_sentences[1]])
+            negative_comments = f"{negative_sentences[0]}, and {negative_sentences[1]}"
             negative_comments = negative_comments.replace("also", "", 1)
             negative_comments = ". ".join([negative_comments, negative_sentences[2]])
         elif (len(negative_sentences) == 2):
-            negative_comments = " ".join([negative_sentences[0], ", and ", negative_sentences[1]])
+            negative_comments = f"{negative_sentences[0]}, and {negative_sentences[1]}"
             negative_comments = negative_comments.replace("also", "", 1)
         elif (len(negative_sentences) == 1):
             negative_comments = negative_sentences[0]
@@ -61,16 +61,24 @@ class CommentGenerator:
         comment = text.format(short_name = self._short_name, pronoun = pronoun, adjective = adjective)
         comment = self.__format_comment(comment)
 
-        return comment
+        if probe:
+            return comment, len(positive_sentences), len(negative_sentences)
+        else:
+            return comment
         
     def __format_comment(self, comment):
         words = nltk.word_tokenize(comment)
 
         for word in words:
             if word == "Moreover" or word == "However":
-                words[words.index(word) - 1] = "."
+                if words[words.index(word) - 1] == ",":
+                    words[words.index(word) - 1] = "."
+                elif words[words.index(word) - 1] == "and":
+                    words[words.index(word) - 2] = "."
+                    words[words.index(word) - 1] = "."
 
-        sentences = " ".join(words).replace(" .", ".").replace("..", ".").replace(" ,", ",").replace("  ", " ").replace(" !", "!")
+        sentences = " ".join(words)
+        sentences = sentences.replace(" .", ".").replace("..", ".").replace(",.", ".").replace(" ,", ",").replace("  ", " ").replace(" !", "!")
         sentences = nltk.sent_tokenize(sentences)
         formatted = [sentence[0].upper() + sentence[1:] for sentence in sentences]
         
@@ -95,17 +103,3 @@ class CommentGenerator:
                         positive_sentences.append(comment)
 
         return positive_sentences, negative_sentences
-
-# TEST
-
-# import pandas as pd
-
-# comment_mapping = pd.read_excel("Z:\Downloads\Grader Report P1A Art Sample.xlsx", sheet_name = "Comment Mapping", index_col = 0, header = 0)
-# student_result = {"Shows good art expression": "C",
-#                   "Has a creative way of using art media": "D",
-#                   "Is able to use art to express ideas and feelings": "D", 
-#                   "Has a good sense of colour which he or she applies to artwork": "D", 
-#                   "Develops creativity": "D"}
-# student_final_grade = 80
-
-# print(CommentGenerator("Yabushita Fu", "Fu", "F", comment_mapping, student_result, student_final_grade).generate_comment())
