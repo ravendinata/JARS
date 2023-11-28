212a911c21408dee75aa6307511a9afd101c3dfb
Raven Limadinata <raven.limadinata@outlook.com>
2023-11-28 08:29:32 +0700

    Added log messages on LTP instance manager

--- processor/helper/language_tool_master.py
+++ processor/helper/language_tool_master.py
@@ -4,14 +4,22 @@ _tool = None
 
 def get_tool():
     """Returns a LanguageTool object."""
+    print("[  ] Looking for LanguageTool instance…")
+    
     global _tool
     if _tool is None:
+        print("[  ] LanguageTool instance not found. Creating new instance…")
         _tool = ltp.LanguageTool('en-UK', config = {"cacheSize": 1000, "pipelineCaching": True, "maxCheckThreads": 20})
+    
+    print("[OK] LanguageTool instance found.")
     return _tool
 
 def close_tool():
     """Closes the LanguageTool object."""
+    print("[  ] Closing LanguageTool instance…")
     global _tool
     if _tool is not None:
         _tool.close()
         _tool = None
+    
+    print("[OK] LanguageTool instance closed.")
