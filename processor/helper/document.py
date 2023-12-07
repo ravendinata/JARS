c8daedbd5c6c2332948b54b8b2e5083422219aa4
Raven Limadinata <raven.limadinata@outlook.com>
2023-12-07 08:44:57 +0700

    Added function to customize table cell border

--- processor/helper/document.py
+++ processor/helper/document.py
@@ -2,6 +2,8 @@ import json
 
 from docx import Document
 from docx.shared import Mm
+from docx.oxml import OxmlElement
+from docx.oxml.ns import qn
 
 import config
 
@@ -18,3 +20,31 @@ def setup_page(document: Document, page_size: str = 'a4'):
     section.bottom_margin = Mm(page_param['margin_bottom'])
 
     return document
+
+@staticmethod
+def set_cell_border(cell, **kwargs):
+    tc = cell._tc
+    tcPr = tc.get_or_add_tcPr()
+
+    # check for tag existnace, if none found, then create one
+    tcBorders = tcPr.first_child_found_in("w:tcBorders")
+    if tcBorders is None:
+        tcBorders = OxmlElement('w:tcBorders')
+        tcPr.append(tcBorders)
+
+    # list over all available tags
+    for edge in ('start', 'top', 'end', 'bottom', 'insideH', 'insideV'):
+        edge_data = kwargs.get(edge)
+        if edge_data:
+            tag = 'w:{}'.format(edge)
+
+            # check for tag existnace, if none found, then create one
+            element = tcBorders.find(qn(tag))
+            if element is None:
+                element = OxmlElement(tag)
+                tcBorders.append(element)
+
+            # looks like order of attributes is important
+            for key in ["sz", "val", "color", "space"]:
+                if key in edge_data:
+                    element.set(qn('w:{}'.format(key)), str(edge_data[key]))
