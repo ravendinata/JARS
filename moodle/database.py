3a53d335f25ae5f40c354ba0541eb05a0310bbe5
Raven Limadinata <raven.limadinata@outlook.com>
2024-01-03 12:56:13 +0700

    Updated method to test connection to Moodle DB

--- moodle/database.py
+++ moodle/database.py
@@ -4,23 +4,33 @@ import config
 
 @staticmethod
 def test_connection():
-    conn = pymysql.connect(
-        host = config.get_config("moodle_db_server"),
-        user = config.get_config("moodle_db_username"),
-        password = config.get_config("moodle_db_password"),
-        db = config.get_config("moodle_db_name"),
-        charset = "utf8mb4",
-        cursorclass = pymysql.cursors.DictCursor
-    )
-
+    """
+    Tests the connection to the moodle database.
+    
+    Returns:
+        bool: Whether the connection was successful.
+    """
     try:
+        conn = pymysql.connect(
+            host = config.get_config("moodle_db_server"),
+            user = config.get_config("moodle_db_username"),
+            password = config.get_config("moodle_db_password"),
+            db = config.get_config("moodle_db_name"),
+            charset = "utf8mb4",
+            cursorclass = pymysql.cursors.DictCursor
+        )   
+
         with conn.cursor() as cursor:
             sql = "SHOW TABLES;"
             cursor.execute(sql)
             result = cursor.fetchall()
             print(result)
-    finally:
-        conn.close()
+    except Exception as e:
+        print(e)
+        return False
+
+    conn.close()
+    return True
 
 class Database:
     def __init__(self):
