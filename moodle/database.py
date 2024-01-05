6b3f60480ff5ffb4b5861305aa45fd27ca336bae
Raven Limadinata <raven.limadinata@outlook.com>
2024-01-05 08:45:34 +0700

    Moved DB connection test configuration check
    The configuration check is now placed inside the try block so the exception is caught by the internal try-catch block.

--- moodle/database.py
+++ moodle/database.py
@@ -14,12 +14,12 @@ def test_connection(supress = True):
     Raises:
         ValueError: If the moodle database credentials are not set.
     """
-    # Check if moodle database credentials are set
-    config_keys = ["moodle_db_server", "moodle_db_username", "moodle_db_name", "moodle_db_password"]
-    if any(config.get_config(key) is None or config.get_config(key) == "" for key in config_keys):
-        raise ValueError("Moodle database credentials and/or server information not set! Please check configuration or contact administrator.")
-
     try:
+        # Check if moodle database credentials are set
+        config_keys = ["moodle_db_server", "moodle_db_username", "moodle_db_name", "moodle_db_password"]
+        if any(config.get_config(key) is None or config.get_config(key) == "" for key in config_keys):
+            raise ValueError("Moodle database credentials and/or server information not set! Please check configuration or contact administrator.")
+    
         with pymysql.connect(
             host = config.get_config("moodle_db_server"),
             user = config.get_config("moodle_db_username"),
