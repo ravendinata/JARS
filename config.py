7f2e619808bcf20f2f04673bd6cabdd1046362e4
Raven Limadinata <raven.limadinata@outlook.com>
2024-06-11 14:02:00 +0700

    Add setting set method for config handler

--- config.py
+++ config.py
@@ -5,3 +5,11 @@ def get_config(config_name):
     with open('config.json') as json_file:
         data = json.load(json_file)
         return data[config_name]
+    
+@staticmethod
+def set_config(config_name, config_value):
+    with open('config.json') as json_file:
+        data = json.load(json_file)
+        data[config_name] = config_value
+        with open('config.json', 'w') as json_file:
+            json.dump(data, json_file, indent = 4)
