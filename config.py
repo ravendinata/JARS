fb26f594e53c03cfc3f20d1eb2a6cd0e8bf5a79a
Raven Limadinata <raven.limadinata@outlook.com>
2024-06-12 09:41:29 +0700

    Separate saving and setting configuration method

--- config.py
+++ config.py
@@ -7,9 +7,18 @@ def get_config(config_name):
         return data[config_name]
     
 @staticmethod
-def set_config(config_name, config_value):
-    with open('config.json') as json_file:
-        data = json.load(json_file)
-        data[config_name] = config_value
-        with open('config.json', 'w') as json_file:
-            json.dump(data, json_file, indent = 4)
+def set_config(config_name, config_value, source = None):
+    if not source:
+        with open('config.json') as json_file:
+            data = json.load(json_file)
+            data[config_name] = config_value
+            return data
+    else:
+        source[config_name] = config_value
+        return source
+
+@staticmethod
+def save_config(data):
+    print("Saving configuration…")
+    with open('config.json', 'w') as json_file:
+        json.dump(data, json_file, indent = 4)
