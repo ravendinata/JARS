13478d9ba6864bd7feee051fc43015d4e884a3f6
Raven Limadinata <raven.limadinata@outlook.com>
2024-06-13 10:12:12 +0700

    Added pyfiglet data and hidden dependencies

--- build_gui.bat
+++ build_gui.bat
@@ -4,6 +4,8 @@ pyinstaller ^
 --hidden-import "babel.numbers" ^
 --hidden-import "windows_toasts" ^
 --hidden-import "winrt.windows.foundation.collections" ^
+--hidden-import "pyfiglet.fonts" ^
+--collect-data "pyfiglet.fonts" ^
 --uac-admin ^
 --name jars ^
 main.py
