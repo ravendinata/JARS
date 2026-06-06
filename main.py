af81f7d5aecb5515a5bb006629c905c6da819768
Raven Limadinata <raven.limadinata@outlook.com>
2026-06-06 10:20:25 +0700

    Update version strings

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ It is meant to be used as a standalone application being more user-friendly
 and easier to use than the console application.
 """
 
-__version__ = "1.3.1"
+__version__ = "1.3.2"
 __author__ = "Raven Limadinata"
 
 import customtkinter as ctk
